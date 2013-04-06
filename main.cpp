174f7890388533dda112f9f813bf4e522513be96
Kohe Tokoi <tokoik@gmail.com>
2013-04-07 01:45:08 +0900

    revert gg.h/gg.cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,67 @@
+#include <iostream>
+
+// 補助プログラム
 #include "gg.h"
 using namespace gg;
 
 //
+// 初期設定
+//
+static void init(void)
+{
+  // 背景色
+  glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+}
+
+//
 // メインプログラム
 //
-int main(int argc, char *argv[])
+int main(int argc, const char * argv[])
 {
-  // ゲームグラフィックス特論の都合にもとづく初期化
-  if (!ggInit()) return 1;
-
-  // 背景色
-  glClearColor(1.0, 1.0, 1.0, 1.0);
-
+  // GLFW を初期化する
+  if (glfwInit() == GL_FALSE)
+  {
+    // 初期化に失敗した
+    std::cerr << "Error: Failed to initialize GLFW." << std::endl;
+    return 1;
+  }
+  
+  // OpenGL Version 3.2 Core Profile を選択する
+  glfwOpenWindowHint(GLFW_OPENGL_VERSION_MAJOR, 3);
+  glfwOpenWindowHint(GLFW_OPENGL_VERSION_MINOR, 2);
+  glfwOpenWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  
+  // ウィンドウを開く
+  if (glfwOpenWindow(0, 0, 0, 0, 0, 0, 0, 0, GLFW_WINDOW) == GL_FALSE)
+  {
+    // ウィンドウが開けなかった
+    std::cerr << "Error: Failed to open GLFW window." << std::endl;
+    return 1;
+  }
+  
+  // 開いたウィンドウに対する設定
+  glfwSwapInterval(1);
+  glfwSetWindowTitle("sample");
+  
+  // 補助プログラムによる初期化
+  ggInit();
+  
+  // 初期設定
+  init();
+  
   // ウィンドウが開いている間くり返し描画する
   while (glfwGetWindowParam(GLFW_OPENED))
   {
     // 画面消去
     glClear(GL_COLOR_BUFFER_BIT);
 
-    // ダブルバッファリング
+    //
+    // ここで OpenGL による描画を行う
+    //
+
+    // バッファを入れ替える
     glfwSwapBuffers();
   }
-
+  
   return 0;
 }
